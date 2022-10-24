676b8e2a06236375db19ae2ff219b43db4f282a0
KamalEmon <u1304013@student.cuet.ac.bd>
2022-10-25 03:06:21 +0600

    Vercel config file created

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000
 
+app.use(cors())
+
+const courses = require('./Data/courses.json');
+const courseDetails = require('./Data/course-detail.json')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(courses)
+})
+
+app.get('/courses',(req,res) => {
+    res.send(courseDetails)
 })
 
 
